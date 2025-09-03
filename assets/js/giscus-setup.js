a659364333e19b2f158c19c004f9caf48ce454ab
tommy-weihao <zhuweihao00@gmail.com>
2025-09-03 16:50:29 +0000

    Deploying to gh-pages from @ tommy-weihao/tommy-weihao.github.io@05c19f6bc07a7e33db09adc93e93cb0817e11c28 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -9,7 +9,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "tommy-weihao/weihaozhu.github.io",
+    "data-repo": "tommy-weihao/tommy-weihao.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
